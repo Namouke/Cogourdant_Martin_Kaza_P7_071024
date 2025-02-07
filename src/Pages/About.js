f0136f53d9d6db59276c8dbcc28d5d787a10eccc
Namouke <79513377+Namouke@users.noreply.github.com>
2025-02-07 18:32:38 +0100

    ajout du css pour le component Banner

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,13 +3,13 @@ import Banner from '../components/Banner.js'
 import Collapse from '../components/Collapse'
 import Data from '../data/collapseAboutData'
 import imgAbo from '../assets/imageAbout.png'
-
+import '../style/pages/_about.scss'
 
 
 const AboutPage = () => {
   return (
     <section className='about'>
-      <Banner img={imgAbo} alt="Image page A propos" />
+      <Banner img={imgAbo} alt="Image page A propos" className="about__banner"/>
       {Data.map((item, index) => (
         <Collapse key={index} title={item.title}>
           <p>{item.content}</p>
