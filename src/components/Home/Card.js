9fcb4f1e583648c5644cb4bac420d12c53952827
Namouke <79513377+Namouke@users.noreply.github.com>
2025-02-08 15:08:59 +0100

    ajout du css et correction du nom des className du component card

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../style/components/home/card.scss"
 
 const card = (props) => {
     const { img, title, alt, id, className = "" } = props;
 
     return (
-        <Link to={`/housing/${id}`} className={`cards ${className}`}>
-            <img src={img} className="cards__img" alt={alt}></img>
-            <p className="cards__title">{title}</p>
+        <Link to={`/housing/${id}`} className={`card ${className}`}>
+            <img src={img} className="card__img" alt={alt}></img>
+            <p className="card__title">{title}</p>
         </Link>
     );
 
